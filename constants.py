08dad86e88188c7a7ea9127ed6cd7d3a9dc04f69
Momchil Tomov <tomov90@gmail.com>
2013-06-15 21:03:44 +0200

    added new database uri...trying to redeploy the app

--- constants.py
+++ constants.py
@@ -1,5 +1,8 @@
 class DatabaseConstants:
-    DATABASE_URI_TEMPLATE = "mysql://ebroot:princetoninblank@aa1rdu5arf3spky.c7vhnm8xo98e.us-east-1.rds.amazonaws.com/%s?init_command=set%%20character%%20set%%20utf8"
+    DATABASE_URI_TEMPLATE = "mysql://ebroot:princetoninblank@aa6fkakt0fky17.c7vhnm8xo98e.us-east-1.rds.amazonaws.com/%s?init_command=set%%20character%%20set%%20utf8"
+    # old ptonin
+    #DATABASE_URI_TEMPLATE = "mysql://ebroot:princetoninblank@aa1rdu5arf3spky.c7vhnm8xo98e.us-east-1.rds.amazonaws.com/%s?init_command=set%%20character%%20set%%20utf8"
+    # local
     #DATABASE_URI_TEMPLATE = "mysql://root:mainatati@localhost/%s?init_command=set%%20character%%20set%%20utf8"
     DATABASE_NAME = 'pton_in'
     DATABASE_URI = DATABASE_URI_TEMPLATE % DATABASE_NAME
